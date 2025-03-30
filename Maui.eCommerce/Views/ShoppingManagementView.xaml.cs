626557f3d9eef65846904408096ed89252fbab41
sandifer2 <zsandifer@icloud.com>
2025-03-30 19:42:16 -0400

    Built out ui and xaml for shopping view, does not work correctly such that when you click shop it does nothing

---
+++ Maui.eCommerce/Views/ShoppingManagementView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.eCommerce.Views;
+
+public partial class ShoppingManagementView : ContentPage
+{
+    public ShoppingManagementView()
+    {
+        InitializeComponent();
+    }
+}
